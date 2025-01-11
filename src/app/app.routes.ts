d477f1e814f702a112610fcd8734b9c96432a964
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-01-12 00:09:36 +0300

    güzelleştirildi.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { NewslatterComponent } from './components/newslatter/newslatter.component';
 
 export const routes: Routes = [
     {
@@ -9,9 +10,14 @@ export const routes: Routes = [
         component:AppComponent
     },
     {
+        path:'haberler',
+        component:NewslatterComponent
+    },
+    {
         path:'login',
         component:LoginComponent
     },
+  
    
   { path: '**', component: NotFoundComponent }
 
---
+++ src/app/components/newslatter/newslatter.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-newslatter',
+  imports: [CommonModule],
+  templateUrl: './newslatter.component.html',
+  styleUrl: './newslatter.component.css'
+})
+export class NewslatterComponent {
+
+}

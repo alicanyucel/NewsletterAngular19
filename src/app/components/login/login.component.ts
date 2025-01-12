c31687cbce9ccc09b6fc68edeae94c5e6230b23d
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-01-12 18:16:06 +0300

    ngx font awesome

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,28 +1,63 @@
+import { AfterContentChecked, Component, OnInit } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
+import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { RouterLink, RouterModule } from '@angular/router';
-import { NgForm, ReactiveFormsModule } from '@angular/forms'
-import { FormsModule } from '@angular/forms';
-import { AuthService } from '../../services/auth.service';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from '../../services/auth.service';  
 import { CheckInputValidDirective } from '../../directives/check-input-valid.directive';
-import {  HttpClient, HttpClientModule } from '@angular/common/http';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { UserModel } from '../../models/usermodel';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterModule,CommonModule,FormsModule,ReactiveFormsModule,CheckInputValidDirective,HttpClientModule],
-  providers:[AuthService],
+  standalone: true,
+  imports: [
+    RouterModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CheckInputValidDirective,
+    HttpClientModule,
+    ToastrModule
+  ],
+  providers: [
+    AuthService,ToastrService
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit,AfterContentChecked {
+  isAuth: boolean = false;
+  userName: string = "";
+  toastr: ToastrService;
+  router: Router;
+
   constructor(
-    private _auth:AuthService
-  ){}
-  ngOnInit():void{}
-  login(loginForm: NgForm){
-  if(loginForm.valid)
-  {
-   this._auth.login(loginForm.value);
-  }
+    private _auth: AuthService
+  ) {}
+  ngAfterContentChecked(): void {
+    this.checkUser();
+   }
+ checkUser(){
+   if(localStorage.getItem("user"))
+   {
+     this.isAuth=true;
+     let user:UserModel=JSON.parse(localStorage.getItem("user"));
+     this.userName=user.nameLastName;
+   }
+   else
+   this.isAuth=false;
+ }
+ logOut(){
+   localStorage.clear();
+   this.toastr.warning("başarıyla çıkış yapıldı");
+   this.router.navigateByUrl("/login");
+ }
+  ngOnInit(): void {}
+
+  login(loginForm: NgForm) {
+    if (loginForm.valid) {
+      this._auth.login(loginForm.value);
+    }
   }
 }

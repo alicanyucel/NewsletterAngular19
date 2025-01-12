1aef7b2b045947871138a5bf88ea86ee9cc76428
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-01-12 11:25:56 +0300

    gerekli yapılar eklendi.

---
+++ src/app/services/swall.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import Swal from 'sweetalert2';
+@Injectable({
+  providedIn: 'root'
+})
+export class SwalService {
+
+  constructor() { }
+
+  callSwal(title:string="",text:string="",btnName:string="Sil")
+  {
+    const swal=Swal.fire({
+      cancelButtonText:"Vazgeç",showCancelButton:true,
+      confirmButtonText:btnName,
+      showConfirmButton:true,
+      title:title,
+      text:text
+
+    });
+    return swal;
+  }
+}
